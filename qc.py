916ef36375b60a6eaa4273703fcf8dd80a4b8bb4
wishva herath <wishvah@gmail.com>
2019-10-26 19:01:49 +1000

    still module 1

--- qc.py
+++ qc.py
@@ -1,40 +1,69 @@
 #reimagining fastqc in python
 #WH
 
-
-
+import numpy as np
 import fileinput
 import sys
 import gzip
 from fastqandfurious import fastqandfurious
 
-# from collections import defaultdict
-# base_qual_dict = defaultdict(list)
+bufsize = 20000
+import collections
+qual_dict = collections.defaultdict(dict)
+#d['dict1']['innerkey'] = 'value'
+
+from collections import defaultdict
+# qual_dict = defaultdict(list)
 #setting up the qual dict
 
-quality_char = [ord(i) for i in """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""]
+quality_char = [ord(i)-33 for i in """!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""]
 quality_value = range(len(quality_char))
 quality_dict = dict(zip(quality_char, quality_value))
 
+#pupulating the dict with zeros
+#for i in quality_char:
+#    base_qual_dict[i] = 0
 
-print(quality_dict)
+#use dict.get(k, default)
 
-
-
-
-
-
-
-
-bufsize = 20000
-base_qual_dict = {}
-def add_base_qual_dict(qual):
+"""
+def add_base_qual_dict2(qual):
+    pos = 0
     for q in qual:
-        if q in base_qual_dict:
-            base_qual_dict[q] = base_qual_dict[q]+ 1
-        else:
-            base_qual_dict[q] = 1
+        counter = dict_pos_qual_count.get(pos, 0)
+        counter = counter + 1
+        dict_pos_qual_count[pos][
 
+        if dict_pos_qual_count[pos][q]:
+            dict_pos_qual_count[pos][q] = dict_pos_qual_count[pos][q] + 1
+        else:
+            dict_pos_qual_count[pos][q] = 1
+        pos = pos + 1
+
+"""
+
+
+def add_base_qual_dict2(qual):
+    pos = 0
+    for q in qual:
+        # key = str(pos) + "_" + str(q)
+        key = (pos*1000) + q
+        #counter = qual_dict.get(key,  0)
+        #counter = counter + 1
+        qual_dict[key] = qual_dict[key] + 1
+        pos = pos + 1
+
+def add_base_qual_dict(qual):
+    pos =0
+
+    for q in qual:
+        if pos in qual_dict:
+            if q in qual_dict[pos]:
+                qual_dict[pos][q] = qual_dict[pos][q] + 1
+            else:
+                qual_dict[pos][q] = 1
+        else:
+            qual_dict[pos][q] = 1
 
 with gzip.open(sys.argv[1].strip()) as fh:
     it = fastqandfurious.readfastq_iter(fh, bufsize, fastqandfurious.entryfunc)
@@ -45,8 +74,14 @@ with gzip.open(sys.argv[1].strip()) as fh:
         add_base_qual_dict(qual)
 
 
+#per base sequence quality
+#Base   Mean    Median  Lower Quartile  Upp    er Quartile  10th Percentile 90th Percentil    e
 
-
+"""
+qual_freq_arr = list(base_qual_dict.values())
+cs = np.cumsum(qual_freq_arr)
+Qn = np.searchsorted(cs, np.percentile(cs, 75)
+"""
 
 
 
