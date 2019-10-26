a22d271b599c9785c3c28bce0d73f5ebb4cf0a43
wishva herath <wishvah@gmail.com>
2019-10-26 22:29:46 +1000

    before adding tile qual

--- qc.py
+++ qc.py
@@ -10,7 +10,10 @@ from fastqandfurious import fastqandfurious
 bufsize = 20000
 import collections
 
+
 qual_dict = collections.defaultdict(dict)
+tile_dict = collections.defaultdict(dict)
+
 #d['dict1']['innerkey'] = 'value'
 
 from collections import defaultdict
@@ -71,7 +74,7 @@ def add_base_qual_dict3(qual):
             qual_dict[pos][q] = 1
         pos = pos + 1
 
-def add_base_qual_dict(qual):
+def add_base_qual_dict(tile, qual):
     pos=0
     for q in qual:
         if pos in qual_dict:
@@ -85,31 +88,41 @@ def add_base_qual_dict(qual):
 with gzip.open(sys.argv[1].strip()) as fh:
     it = fastqandfurious.readfastq_iter(fh, bufsize, fastqandfurious.entryfunc)
     for entry in it:
-        header = entry[0]
+        header = str(entry[0])
+        #extract tile numer
+        tile = header.strip().split(":")[4].strip()
+        print(tile)
         seq = [i for i in entry[1]]
         qual = [quality_dict[i] for i in entry[2]]
-        add_base_qual_dict(qual)
+
+        add_base_qual_dict(tile, qual)
 
 
-#per base sequence quality
-#Base   Mean    Median  Lower Quartile  Upp    er Quartile  10th Percentile 90th Percentil    e
+
+print("per base sequence quality")
+print("Base   Mean    Median  Lower Quartile  Upper Quartile  10th Percentile 90th Percentile")
 """
 qual_freq_arr = list(base_qual_dict.values())
 cs = np.cumsum(qual_freq_arr)
 Qn = np.searchsorted(cs, np.percentile(cs, 75)
 """
-data = {}
 for p in qual_dict.keys():
-    print(p)
+    # print(p)
     quality_array = np.array(list(qual_dict[p].values()))
-    print(quality_array)
+    # print(quality_array)
     cs = np.cumsum(quality_array)
-    print(cs)
+    # print(cs)
     q_list = []
     for i in [10, 25, 50, 75, 90]:
         q_list.append(np.searchsorted(cs, np.percentile(cs,i)))
 
-    print(p, "->", q_list)
-    data[p] = q_list
+    # print(p, "->", q_list)
+    #calculate mean
+    quality_key = np.array(list(qual_dict[p].keys()))
+    total = np.sum(quality_key*quality_array)
+    quality_mean = total / np.sum(quality_array)
+
+    print(p, quality_mean, q_list[0], q_list[1], q_list[2], q_list[3])
+
 
 
