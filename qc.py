cc7577bdabaedbcbd19abdb5a44f7913438f0c14
wishva herath <wishvah@gmail.com>
2019-10-28 06:27:03 +1000

    kmers to go

--- qc.py
+++ qc.py
@@ -1,6 +1,7 @@
 #reimagining fastqc in python
 #WH
 
+import zlib
 import numpy as np
 import fileinput
 import sys
@@ -58,7 +59,6 @@ def add_base_qual_dict2(qual):
         # key = str(pos) + "_" + str(q)
         key = (pos*1000) + q
         #counter = qual_dict.get(key,  0)
-        #counter = counter + 1
         qual_dict[key] = qual_dict[key] + 1
         pos = pos + 1
 
@@ -121,10 +121,30 @@ g_pos_count = collections.defaultdict(int)
 c_pos_count = collections.defaultdict(int)
 n_pos_count = collections.defaultdict(int)
 
+length_dict = collections.defaultdict(int)
+
+dedup_list = []
+dedup_dict = collections.defaultdict(int)
+dedup_original_dict={}
+def dedup(bseq):
+    k = zlib.crc32(bseq)
+    if do_dedup:
+        #populate dedup_dict:
+        if k not in dedup_list:
+            dedup_list.append(k)
+            dedup_original_dict[k] = bseq
+    else:
+        #check for dedup
+        if k in dedup_list:
+            dedup_dict[k] = dedup_dict[k] + 1
+
+
 def base_level(seq):
+    l = len(seq)
+    length_dict[l] = length_dict[l] + 1
     pos = 0
     for b in seq:
-        b = chr(b)
+        # b = chr(b)
         if b == 'A':
             a_pos_count[pos] = a_pos_count[pos] + 1
         elif b == 'T':
@@ -139,20 +159,27 @@ def base_level(seq):
 
 #todo for dict entries with default int they start with 0 not 1. So make sure you add one when processing them in the end. 
 
-
+read_count = 0
+do_dedup = True
 with gzip.open(sys.argv[1].strip()) as fh:
     it = fastqandfurious.readfastq_iter(fh, bufsize, fastqandfurious.entryfunc)
     for entry in it:
+        read_count = read_count + 1
+        if read_count >100000:
+            do_dedup=False
+
+        # print(entry[1][10])
         header = str(entry[0])
         #extract tile numer
         tile = header.strip().split(":")[4].strip()
         # print(tile)
-        seq = [i for i in entry[1]]
+        seq = "".join([chr(i) for i in entry[1]])
         qual = [quality_dict[i] for i in entry[2]]
 
         add_base_qual_dict(tile, qual)
         avg_qual_count(qual)
         base_level(seq)
+        dedup(entry[1])
 
 
 
@@ -209,4 +236,26 @@ for p in sorted(a_pos_count):
 
     print(p, g/ff, a/ff, t/ff, c/ff)
 
+print("Per sequence GC content")
+print("GC Content   count")
+for p in sorted(a_pos_count):
+    a = a_pos_count[p]
+    t = t_pos_count[p]
+    g = g_pos_count[p]
+    c = c_pos_count[p]
+    n = n_pos_count[p]
+
+    gc = (g+c)/(a+t) * 100
+    print(p, gc)
+
+print("Per base N content")
+print("Base N-Count")
+for p in sorted(n_pos_count):
+    print(p, n_pos_count[p])
+
+print("Sequence Length Distribution")
+print("Length   Count")
+
+for l in sorted(length_dict):
+    print(l, length_dict[l])
 
