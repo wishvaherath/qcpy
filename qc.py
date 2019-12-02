f6e7db814c13ce9fa70d65d9bbe652b8b0c2830b
wishva herath <wishvah@gmail.com>
2019-12-02 17:34:20 +1000

    multiprocessing

--- qc.py
+++ qc.py
@@ -1,6 +1,6 @@
 #reimagining fastqc in python
 #WH
-import pandas
+
 import time
 import collections
 import queue
@@ -19,46 +19,57 @@ from collections import defaultdict
 import dnaio
 
 
-
 def do_qc(in_data):
-    # print("called")
+
+    #building qual_dict[qual_char] = qual_value to convert qual values to scores
     qd = {}
     quality_char = [i for i in """!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""]
     quality_value = range(len(quality_char))
     quality_dict = dict(zip(quality_char, quality_value))
 
-
     for i in quality_dict.values():
         qd[i] = 0
 
 
-    def add_base_qual_dict(tile_listz, qual_listz):
+    def add_base_qual_dict(header_list, qual_list):
+        """
+        Propogates the following
+        1. tile_sum_dict[tile][pos] = sum quality
+        2. tile_count_dict[tile][pos] = counts
+        3. qual_dict[pos][quality] = count
+
+        returns a list of [1.,2.,3.]
+
+        """
         qual_dict = collections.defaultdict(dict)
         tile_sum_dict = collections.defaultdict(dict)
         tile_count_dict = collections.defaultdict(dict)
 
-        for tile,qual in zip(tile_listz, qual_listz):
-            pos=0
+        for tile,qual in zip(tile_list, qual_list):
+            pos = 0
             for q in qual:
                 if pos in tile_count_dict[tile]:
-                    #if pos is in the tile dict then IS in the qual_dict
+                    #we have seen this pos before
                     tile_sum_dict[tile][pos] = tile_sum_dict[tile][pos] + q
                     tile_count_dict[tile][pos] = tile_count_dict[tile][pos] + 1
                     qual_dict[pos][q] = qual_dict[pos][q] + 1
                 else:
+                    #this pos is new
                     tile_sum_dict[tile][pos] = q
                     tile_count_dict[tile][pos] = 1
                     qual_dict[pos] = qd.copy()
                     qual_dict[pos][q] = 1
                 pos = pos + 1
+        return (qual_dict, tile_sum_dict, tile_count_dict)
 
-        return [qual_dict, tile_sum_dict, tile_count_dict]
 
-
-    def avg_qual_count(qual_listz):
+    def avg_qual_count(qual_list):
+        """
+        Propogates avg_qual_count_dict[mean_quality_of_read] = count
+        """
 
         avg_qual_count_dict = {}
-        for qual in qual_listz:
+        for qual in qual_list:
             q = np.array(qual)
             m = int(np.mean(q))
             if m in avg_qual_count_dict:
@@ -66,36 +77,49 @@ def do_qc(in_data):
             else:
                 avg_qual_count_dict[m] = 1
 
-        return [avg_qual_count_dict]
+        return avg_qual_count_dict
 
 
-    def dedup(bseq):
-        dedup_list = []
+    def dedup(seq_list, bloom_start=0, bloom_stop=50):
+        """
+        Propogates dedup_dict[sequence_key] = count for duplicated sequences.
+        sequence_key = seq[bloom_start:bloom_stop]
+
+        """
         dedup_dict = collections.defaultdict(int)
-        dedup_original_dict={}
 
         dedup_sorted_list = SortedList()
 
+        for seq in seq_list:
+            seq_key = seq[bloom_start:bloom_stop]
+            if seq_key not in dedup_bloom:
+                # the sequence is not a duplicate
+                dedup_dict[seq_key] = dedup_dict[seq_key] + 1
 
         # k = zlib.crc32(bseq)
-        k = bseq[0:50]
-        if do_dedup:
-            #populate dedup_dict:
-            if k not in dedup_list:
-                dedup_bloom.add(k)
-                # dedup_list.append(k)
-                # dedup_original_dict[k] = bseq
-        else:
-            #check for dedup
-            if k not in dedup_bloom:
-                pass
-            else:
+        #k = bseq[0:50]
+        #if do_dedup:
+        #    #populate dedup_dict:
+        #    if k not in dedup_list:
+        #        dedup_bloom.add(k)
+        #        # dedup_list.append(k)
+                ## dedup_original_dict[k] = bseq
+        #else:
+            ##check for dedup
+            #if k not in dedup_bloom:
+                #pass
+            #else:
                 # dedup_dict[k] = dedup_dict[k] + 1
-                dedup_sorted_list.add(k)
+                #dedup_sorted_list.add(k)
 
 
 
     def base_level(seq_listz):
+        """
+        propogates the <base>_pos_count[pos] = count dicts. 
+        and 
+        length_dict[length] = count
+        """
 
         a_pos_count = collections.defaultdict(int)
         t_pos_count = collections.defaultdict(int)
@@ -123,35 +147,36 @@ def do_qc(in_data):
                     n_pos_count[pos] = n_pos_count[pos] + 1
                 pos = pos + 1
 
-        return [a_pos_count, t_pos_count, g_pos_count, c_pos_count, n_pos_count, length_dict]
+        return (a_pos_count, t_pos_count, g_pos_count, c_pos_count, n_pos_count, length_dict)
 
     if in_data =="EMPTY":
         print('done')
         #sys.exit(0)
-    (seq_listz, qual_listz, tile_listz) = in_data
+    (seq_list, qual_list, header_list, tile_list) = in_data
     # print("sent")
-
-    #return [add_base_qual_dict(tile_listz, qual_listz), avg_qual_count(qual_listz), base_level(seq_listz)]
-
-    # print(len(add_base_qual_dict(tile_listz, qual_listz) + avg_qual_count(qual_listz) + base_level(seq_listz)))
-    return add_base_qual_dict(tile_listz, qual_listz) + avg_qual_count(qual_listz) + base_level(seq_listz)
+    return [add_base_qual_dict(tile_list, qual_list), avg_qual_count(qual_list), base_level(seq_list), dedup(seq_list)]
 
 
 
+def read_fastq(fn, chunk_size = 10000 ,bloom_limit = 100000, bloom_start = 0, bloom_stop = 50):
+    """
+    read given fastq.gz file (fn) and put chunks of reads into data_queue as (seq_list, qual_list, header_list)
 
+    fn : file name
+    chunk_size : max sequences per chunk in dna_queue
+    bloom_limit: sequences needs to be added into the bloom filter
 
-
-def read_fastq(fn):
+    """
 
     quality_char = [i for i in """!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""]
     quality_value = range(len(quality_char))
     quality_dict = dict(zip(quality_char, quality_value))
 
-
     tile_list = []
     seq_list = []
     qual_list = []
-    count = 0   
+    header_list = []
+    count = 0
     read_count = 0
     add_bloom = False
 
@@ -166,40 +191,42 @@ def read_fastq(fn):
             qual = [quality_dict[i] for i in entry.qualities]
 
             tile_list.append(tile)
+            header_list.append(entry.name)
             seq_list.append(seq)
             qual_list.append(qual)
 
 
             count = count + 1
             read_count = read_count + 1
-            if read_count > 1000:
+
+            #propogating the bloom filter
+            if read_count > bloom_limit:
                 #this is the sample used for the dedup
                 add_bloom=False
+                #resetting dedup_list to free memory
+                dedup_list = []
+
 
             if add_bloom:
-                k = seq[0:50]
+                k = seq[bloom_start:bloom_length]
                 if k not in dedup_list:
                     dedup_bloom.add(k)
                     dedup_list.append(k)
-            if count == 1000:
-                # print("dispatching")
-                data_queue.put((seq_list, qual_list, tile_list))
-
-                # add_base_qual_dict(tile_list, qual_list)
-                # avg_qual_count(qual_list)
-                # base_level(seq_list)
-                # dedup(seq_list)
-
+            if count == chunk_size:
+                # chunk size was hit. Adding the chunk to data_queue
+                data_queue.put((seq_list, qual_list, header_list, tile_list))
+                #reset chunk count and lists
                 count = 0
                 tile_list = []
                 seq_list = []
                 qual_list = []
 
-    print("sending empty")
+    print("File read complete")
+    #The EMPTY flag denotes the end of the read.
     data_queue.put("EMPTY")
-
-    #push the final dataset
+    #exiting thread
     sys.exit(1)
+
 
 
 def join_dict(one,two, level=0):
@@ -252,7 +279,6 @@ def merge_results(m_results, results):
 
 
 if __name__ == "__main__":
-    #todo for dict entries with default int they start with 0 not 1. So make sure you add one when processing them in the end. 
 
     m_qual_dict = collections.defaultdict(dict)
     m_tile_sum_dict = collections.defaultdict(dict)
@@ -264,57 +290,78 @@ if __name__ == "__main__":
     m_c_pos_count = collections.defaultdict(int)
     m_n_pos_count = collections.defaultdict(int)
     m_length_dict = collections.defaultdict(int)
+    dedup_dict = collections.defaultdict(int)
+    m_results = [m_qual_dict, m_tile_sum_dict, m_tile_count_dict, m_avg_qual_count_dict, m_a_pos_count, m_t_pos_count, m_g_pos_count, m_c_pos_count,m_n_pos_count, m_length_dict, dedup_dict]
 
 
+    #todo for dict entries with default int they start with 0 not 1. So make sure you add one when processing them in the end. 
 
-    m_results = [m_qual_dict, m_tile_sum_dict, m_tile_count_dict, m_avg_qual_count_dict, m_a_pos_count, m_t_pos_count, m_g_pos_count, m_c_pos_count, m_n_pos_count, m_length_dict]
-
-
-
-
-
-
+    #Setting up bloom filter
     dedup_bloom = pybloomfilter.BloomFilter(100010, 0.1, "dedup.bloom")
+
     read_count = 0
     add_bloom = True
     dedup_list = []
     count = 0
+    results = []
 
-    # tile_list = []
-    # seq_list = []
-    # qual_list = []
-
-    pool = multiprocessing.Pool(processes=4)
+    #setting up the thread to read fastq
+    pool = multiprocessing.Pool(processes=2)
     data_queue = queue.Queue()
-
-
     print("initiating")
     reader = Thread(target=read_fastq, args = (sys.argv[1].strip(),))
     # reader.setDaemon(True) #change this to process if you want
     reader.start()
+
     try_to_stop = False
     data_buffer = []
+
     while True:
-        # time.sleep(1)
+        time.sleep(1)
         if data_queue.qsize() > 0:
             #there is stuff in the queue
             temp_data = data_queue.get()
 
             if "EMPTY" in temp_data:
+                print("lengthsdfadsfasd", len(results))
                 print("empty detected in main loop")
                 if len(data_buffer) > 0:
-                    results = poolmap(do_qc, data_buffer)
 
-                    m_results = merge_results(m_results, results)
-                break
+                    # results = poolmap(do_qc, data_buffer)
+                    results.append(poolmap(do_qc, data_buffer))
+
+                    #m_results = merge_results(m_results, results)
+                    sys.exit(0)
+                    break
+                else:
+                    sys.exit(0)
             else:
                 data_buffer.append(temp_data)
-
                 if len(data_buffer) == 4:
                     results_4 = pool.map(do_qc, data_buffer)
                     for r in results_4:
-                        m_results = merge_results(m_results, r)
+                        #m_results = merge_results(m_results, r)
+                        results.append(r)
                     data_buffer = []
+
+
+
+    #while True:
+    #    if data_queue.qsize() > 0:
+    #        #there is stuff in the queue
+    #        temp_data = data_queue.get()
+
+    #        if "EMPTY" in temp_data:
+    #            print("empty detected in main loop")
+    #            if len(data_buffer) > 0:
+    #                results = pool.map(do_qc, data_buffer)
+    #            break
+    #        else:
+                #data_buffer.append(temp_data)
+                ##wait until there are 4 chunks in the data_buffer
+                #if len(data_buffer) == 4:
+                #    results = pool.map(do_qc, data_buffer)
+                #    data_buffer = []
 
 
 
@@ -343,6 +390,7 @@ if __name__ == "__main__":
 
 
 
+print(len(results))
 sys.exit(1)
 
 
